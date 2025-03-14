1ce7787614d27ba130154cba55bd205a62a47f66
irobust <p_permpimon@hotmail.com>
2025-03-15 00:45:36 +0700

    Update setup.sh

--- 06-Docker-and-Docker-compose/Weather.Api/sql/setup.sh
+++ 06-Docker-and-Docker-compose/Weather.Api/sql/setup.sh
@@ -1,3 +1,3 @@
 # Wait for SQL Server to be started and then run the sql script
 ./wait-for-it.sh localhost:1433  --timeout=0 --strict -- sleep 10s && \
-/opt/mssql-tools/bin/sqlcmd -S localhost -i InitializeDatabase.sql -U sa -P "$SA_PASSWORD"
+/opt/mssql-tools18/bin/sqlcmd -S localhost -i InitializeDatabase.sql -U sa -P "$SA_PASSWORD"
